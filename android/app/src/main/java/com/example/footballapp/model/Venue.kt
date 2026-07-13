ef441731cd3ac4524d49c738ba57409971714aa0
dfs1972 <dfs1972@outlook.com>
2026-07-13 10:36:00 +0100

    Commit some untracked files.

---
+++ android/app/src/main/java/com/example/footballapp/model/Venue.kt
@@ -0,0 +1,10 @@
+package com.example.footballapp.model
+
+data class Venue(
+    val id: Int = 0,
+    val name: String? = null,
+    val address: String? = null,
+    val city: String? = null,
+    val capacity: Int = 0,
+    val surface: String? = null
+)
